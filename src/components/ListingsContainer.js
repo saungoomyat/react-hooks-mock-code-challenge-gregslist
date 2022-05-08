3339d8dffa508b65d02afa3013ea4555e4ecc3e9
Saung Oo Myat <saung.myat@gmail.com>
2022-05-08 16:26:01 -0400

    commit tree 7daecab75d950598687af2912063e71f6fde8e1f

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,12 +1,54 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
+import NewListingForm from "./NewListingForm";
 
-function ListingsContainer() {
+function ListingsContainer({ search }) {
+  const [listings, setListings] = useState([]);
+  const [sortBy, setSortBy] = useState("id");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/listings")
+      .then((r) => r.json())
+      .then((listings) => setListings(listings));
+  }, []);
+
+  function handleDeleteListing(id) {
+    const updatedListingsArray = listings.filter(
+      (listing) => listing.id !== id
+    );
+    setListings(updatedListingsArray);
+  }
+
+  function handleAddListing(newListing) {
+    const updatedListingsArray = [newListing, ...listings];
+    setListings(updatedListingsArray);
+  }
+
+  const listingCards = listings
+    .filter((listing) =>
+      listing.description.toLowerCase().includes(search.toLowerCase())
+    )
+    .sort((listingA, listingB) => {
+      if (sortBy === "id") {
+        return listingA.id - listingB.id;
+      } else {
+        return listingA.location.localeCompare(listingB.location);
+      }
+    })
+    .map((listingObj) => (
+      <ListingCard
+        key={listingObj.id}
+        listing={listingObj}
+        onDeleteListing={handleDeleteListing}
+      />
+    ));
+
   return (
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
+      <NewListingForm onAddListing={handleAddListing} />
+      <button onClick={() => setSortBy("id")}>Sort By Default</button>
+      <button onClick={() => setSortBy("location")}>Sort By Location</button>
+      <ul className="cards">{listingCards}</ul>
     </main>
   );
 }
